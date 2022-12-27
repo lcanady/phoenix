94c5da60cfda35eba1ca3069a8d55f2574c33c93
Lem Canady <lem.canady@gmail.com>
2022-12-27 10:38:21 -0800

    better error handling

--- src/index.ts
+++ src/index.ts
@@ -74,3 +74,8 @@ process.on("SIGINT", async () => {
 
   process.exit();
 });
+
+process.on("unhandledRejection", (reason, p) => {
+  console.log("Unhandled Rejection at: Promise", p, "reason:", reason);
+  // application specific logging, throwing an error, or other logic here
+});
