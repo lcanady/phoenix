31d5a02f3e05e81500914e3efd319b08b391b2d9
Lem Canady <lem.canady@gmail.com>
2023-01-12 08:04:08 -0800

    file upload bug

--- src/app.ts
+++ src/app.ts
@@ -68,7 +68,7 @@ const upload = multer({ storage: diskStorage });
 
 app.post("/upload", auth, upload.single("img"), (req, res, next) => {
   const file = req.file;
-  if (!file) next(new Error("No file uploaded!"));
+  if (!file) return next(new Error("No file uploaded!"));
   res.status(200).json({ file: file?.filename });
 });
 
